777ef0d9da24e73725fe3b73d1e1f1430a5d5f8d
william72425 <125339420+william72425@users.noreply.github.com>
2026-05-01 19:48:13 +0630

    Update index.js (google search key update)

--- src/pages/index.js
+++ src/pages/index.js
@@ -189,10 +189,11 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Digital Hub Myanmar - Hubby Store</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700;800;900&family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
-      </Head>
+  <meta name="google-site-verification" content="abCJLqbY2J_i8FU3SwX_NOkkh5RlESldCaTbfFvojjk" />
+  <title>Digital Hub Myanmar - Hubby Store</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+  <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700;800;900&family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
+</Head>
 
       <div className={`min-h-screen transition-all duration-300 relative overflow-x-hidden ${
         isDarkMode 
